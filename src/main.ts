933fc5bed6653d0817a4d349d9be2ceca3ce9a6f
RaJharit77 <hei.rajo.2@gmail.com>
2025-11-29 15:43:46 +0300

    chore: Update main file

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, BadRequestException } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { CustomLogger } from './common/logger/custom-logger.service';
 import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
@@ -9,7 +9,27 @@ async function bootstrap() {
     logger: new CustomLogger(),
   });
 
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+    forbidNonWhitelisted: true,
+    transformOptions: {
+      enableImplicitConversion: true,
+    },
+    exceptionFactory: (errors) => {
+      const messages = errors.map(error => {
+        return {
+          property: error.property,
+          constraints: error.constraints,
+        };
+      });
+      return new BadRequestException({
+        message: 'Validation failed',
+        errors: messages,
+      });
+    },
+  }));
+
   app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(process.env.PORT || 3000);
