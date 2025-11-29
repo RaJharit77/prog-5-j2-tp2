8e43895373121680b6119dedd284245695e51b73
RaJharit77 <hei.rajo.2@gmail.com>
2025-11-29 15:41:57 +0300

    feat: Update renter dto

--- src/modules/renter/dto/create-renter.dto.ts
+++ src/modules/renter/dto/create-renter.dto.ts
@@ -4,25 +4,32 @@ import {
     IsEnum,
     IsOptional,
     IsPhoneNumber,
+    MinLength,
+    MaxLength,
 } from 'class-validator';
 import { RenterType } from '../enums/renter-type.enum';
 
 export class CreateRenterDto {
     @IsString()
+    @MinLength(2)
+    @MaxLength(255)
     name!: string;
 
     @IsEmail()
     email!: string;
 
-    @IsString()
     @IsOptional()
+    @IsString()
     @IsPhoneNumber()
     phone?: string;
 
-    @IsEnum(RenterType)
+    @IsEnum(RenterType, { 
+        message: `Type must be one of: ${Object.values(RenterType).join(', ')}` 
+    })
     type!: RenterType;
 
-    @IsString()
     @IsOptional()
+    @IsString()
+    @MaxLength(500)
     address?: string;
 }
